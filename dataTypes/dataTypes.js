6d0a696b816a8c3c805efd00a9e18712de617570
Nishan Das <nishan.sid@gmail.com>
2021-02-09 16:09:59 +0600

    Datatype done

--- dataTypes/dataTypes.js
+++ dataTypes/dataTypes.js
@@ -7,6 +7,29 @@ let n =45;
 console.log(typeof n); 
 
 //String
-
 let t = "hello world";
 console.log (typeof t); 
+
+//NULL
+let ak = null;
+console.log(ak);
+
+//Symbol
+
+let k = Symbol();
+console.log(k);
+
+
+//Complex DataType
+
+
+//object
+
+let obj = {
+         Name : "Nathan",
+         Age: 24
+}
+console.log(typeof obj);
+
+let arr = ["ak",25,"jack",50];
+console.log(typeof arr);
