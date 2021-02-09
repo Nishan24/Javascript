2d1035a0f1e80badf8bd5ad5cbe47136219d65fd
Nishan Das <nishan.sid@gmail.com>
2021-02-09 15:19:31 +0600

    second commit variables

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
+//variablr
 
-var n = "45";
+/*
+//var global function scoped
 
+var n = "Nishan";
 console.log(n);
+
+// let function scoped
+
+let age =24;
+console.log(age);
+
+// Example
+
+let input = prompt ("What's your name?");
+alert("HeLLo " + input);
+
+//initial
+
+let address = "Dhaka";
+console.log (address); 
+
+*/
+//What i can use in variable => Letter,Numnber,_,$
+
+const place ="Dhaka";
+place ="Ctg";
+console.log(place);
