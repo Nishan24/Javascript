1edcb5ba9eb6576941474a93ad0bd9f3c4ed5c4a
Nishan Das <nishan.sid@gmail.com>
2021-02-09 17:23:03 +0600

    Type Conversion

---
+++ typeConversion/conversion.js
@@ -0,0 +1,39 @@
+//Number to string
+
+let num = String (555);
+num = String(5+5);
+
+//Date to String
+num = String (new Date());
+
+//Boolean to String
+
+num = String(true);
+
+//Array to String
+
+num = String([1,2,3]);
+
+//Another to tostring method
+
+num = (55).toString();
+
+//String to Number
+
+ num = Number(55);
+ 
+ //boolean to number
+
+ num = Number(true);
+
+ //Another way
+
+ num = parseInt("55");
+ num = parseFloat("55.54556546345");
+
+
+//output
+console.log(num);
+console.log(typeof num);
+//console.log(num.length);
+console.log(num.toFixed(5));
