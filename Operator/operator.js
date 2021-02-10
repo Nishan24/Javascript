ef2461e4e4131b33de4e3364320fa9eb0f5add48
Nishan Das <nishan.sid@gmail.com>
2021-02-10 10:51:44 +0600

    opt

---
+++ Operator/operator.js
@@ -0,0 +1,58 @@
+// let value;
+// let num1 =10;
+// let num2 =5;
+
+// //Simple math
+
+// value = num1 +num2;
+// value = num1 -num2;
+// value = num1 *num2;
+// value = num1 / num2;
+// value = num1 % num2;
+
+// //Math Object
+
+// value = Math.PI;
+// value = Math.E;
+// value = Math.round(452.3456455);
+// value = Math.ceil(7.3);
+// value = Math.floor(7.6);
+// value = Math.sqrt(16);
+// value = Math.pow(2,3);
+// value = Math.abs(-3);
+// value = Math.max(8,54,1,545,97,5,2,545);
+// value = Math.min(8,54,1,545,97,5,2,545);
+// value = Math.random()*100;
+// value = Math.floor(value)+1;
+
+
+
+// //Output
+
+// console.log(value);
+
+
+
+
+///LOVE CALCULATOR/////
+
+
+let n1 = prompt("What's your name?");
+let n2 = prompt("what's your partner name?");
+
+let val = Math.random() * 100;
+val = Math.floor(val) + 1;
+
+//Output
+
+alert (n1 + " and " + n2 + " Love percentage is : "+ val +"%");
+
+
+
+
+
+
+
+
+
+
