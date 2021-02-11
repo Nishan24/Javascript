1b356184d3ad64ad46cf9f929bd0907ad51afcbb
Nishan Das <nishan.sid@gmail.com>
2021-02-11 13:01:51 +0600

    General loop

---
+++ Loops/loops.js
@@ -0,0 +1,48 @@
+// //for loop
+
+// // for(let i=0;i<10; i -= -1){
+// //  console.log(i);   
+
+// // }
+
+// let arr = ["one","two","three","four","five"];
+
+// for(let i = arr.length-1;i>=0; i--){
+    
+
+
+// console.log(arr[i]);
+// }
+
+// //while loop
+
+// let i = 0;
+// while (i<arr.length) {
+//     console.log(arr[i]);
+//     i++;
+    
+// }
+
+//for vs while
+
+// let message= Number(prompt("Enter a number"));
+
+
+// while (message<10) {
+//     alert("Please enter a larger number!!");
+//     message= Number(prompt("Enter a number"));
+
+// }
+// if (message>=10) {
+//     alert("Congratulations");
+    
+// }
+
+let arr = ["one","two","three","four","five"];
+
+arr.forEach(function(elemnent){
+
+
+
+    console.log(elemnent);
+})
